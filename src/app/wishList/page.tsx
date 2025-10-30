09effef63a597866ce079b1677f229f50b505809
Mindi1234 <mimikenig1@gmail.com>
2025-10-30 10:35:47 +0200

    c

--- src/app/wishList/page.tsx
+++ src/app/wishList/page.tsx
@@ -1,6 +1,5 @@
 'use client'; 
 import Link from 'next/link';
-import { Product } from '@/app/types/product';
 import styles from './page.module.css';
 import { useWishlistStore } from '../store/useWishListStore';
 
